8a7e05ba037302c4d138f0e653a2ab4574561058
Karl Ojarand <karl.ojarand@khk.ee>
2020-01-08 09:26:45 +0200

    modified plugin `Very First Plugin` version 1.0

--- wp-content/plugins/very-first-plugin/file.php
+++ wp-content/plugins/very-first-plugin/file.php
@@ -6,4 +6,9 @@
 * Author: Karl Ojarand
 * Author URI: http://ojarandkarl.ikt.khk.ee/wordpress/
 * Version: 1.0
-*/
+**/
+
+function dh_modify_read_more_link() {
+ return '<a class="more-link" href="' . get_permalink() . '">Click to Read!</a>';
+}
+add_filter( 'the_content_more_link', 'dh_modify_read_more_link' );
