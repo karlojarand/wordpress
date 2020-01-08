02f120950defbbf339d03b09cf017d29037e409c
Karl Ojarand <karl.ojarand@khk.ee>
2020-01-08 09:11:29 +0200

    modified plugin `Very First Plugin` version 1.0 (after activation of `Very First Plugin` version 1.0)

---
+++ wp-content/plugins/very-first-plugin/file.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: Very First Plugin
+* Plugin URI: http://ojarandkarl.ikt.khk.ee/wordpress/
+* Description: This is my first WordPress Plugin
+* Author: Karl Ojarand
+* Author URI: http://ojarandkarl.ikt.khk.ee/wordpress/
+* Version: 1.0
+*/
